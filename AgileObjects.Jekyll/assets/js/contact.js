7d64cebe6c17180e6e100297b9d587eaa5999c92
Steve Wilkes <steve@agileobjects.co.uk>
2020-04-25 13:14:29 +0100

    Fixes

--- AgileObjects.Jekyll/assets/js/contact.js
+++ AgileObjects.Jekyll/assets/js/contact.js
@@ -1,10 +1,10 @@
 (function ($) {
     (function (ao) {
         (function (web) {
-            var ContactForm = function() {
-                function contactForm() {};
+            var ContactForm = function () {
+                function contactForm() { };
 
-                contactForm.prototype.handleSend = function(url) {
+                contactForm.prototype.handleSend = function (url) {
                     if (!$getById('contact-form').valid()) {
                         return false;
                     }
@@ -13,9 +13,9 @@
                     this._$showPopupPanel('sending').hide().removeClass('hidden').fadeIn();
 
                     var formData = {
-                        name: getById('name').value,
-                        email: getById('email').value,
-                        message: getById('message').value
+                        name: getById('contact-form-name').value,
+                        email: getById('contact-form-email').value,
+                        message: getById('contact-form-message').value
                     };
                     var that = this;
                     $.ajax(url,
@@ -23,28 +23,28 @@
                             type: 'POST',
                             async: true,
                             data: JSON.stringify(formData)
-                        }).fail(function() {
-                        that._$showPopupPanel('sent-error');
-                    }).done(function() {
-                        that._$showPopupPanel('sent-ok');
-                    });
+                        }).fail(function () {
+                            that._$showPopupPanel('sent-error');
+                        }).done(function () {
+                            that._$showPopupPanel('sent-ok');
+                        });
                     return false;
                 };
 
-                contactForm.prototype.handleSent = function(sent) {
+                contactForm.prototype.handleSent = function (sent) {
                     this._$getFormElements().prop('disabled', false);
                     sent ? document.location.href = '/' : this._$getPopupPanel().fadeOut();
                 };
 
-                contactForm.prototype._$getFormElements = function() {
+                contactForm.prototype._$getFormElements = function () {
                     return $('#contact-form input,#contact-form textarea,#contact-form-submit');
                 };
 
-                contactForm.prototype._$getPopupPanel = function() {
+                contactForm.prototype._$getPopupPanel = function () {
                     return $getById('popup');
                 }
 
-                contactForm.prototype._$showPopupPanel = function(contentId) {
+                contactForm.prototype._$showPopupPanel = function (contentId) {
                     return this._$getPopupPanel().html($getById(contentId).html());
                 };
 
